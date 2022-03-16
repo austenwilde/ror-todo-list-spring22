5bd8ff193f9b5ae54ff106d51216b725d021d732
Austen Wilde <sawilde15@gmail.com>
2022-03-15 20:50:16 -0600

    note backend done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
- 
+  
   namespace :api do
     resources :lists do
       resources :todos
-    end 
+    end
+
+    resources :todos, execept: [:index, :show, :update, :create, :destroy] do
+      resources :notes 
+    end
+
+    resources :notes, execept: [:index, :show, :update, :create, :destroy] do
+      resources :comments 
+    end
+
   end
-  
+
 end
